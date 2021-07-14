daae2f7b7b75228cfdfd36c187c849ab2f748c52
Pahen <knopstany228@gmail.com>
2021-07-15 00:13:47 +0300

    0,0,03

--- main.py
+++ main.py
@@ -10,16 +10,52 @@ import cv2 as cv
 
 directory = 'temple/'
 dict = {}
-for image in os.listdir(directory):
-    img2 = cv.imread(directory+image)
-    cv2.imshow('test', img2)
-    cv2.waitKey(0)
-    img3 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
-    rt, threshhold = cv.threshold(img3, 101,255,0)
-    contur, hr = cv.findContours(threshhold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    arr = [contur[1].size, contur[1].min, contur[1].max , 255,0,0]
-    dict[image] = arr
+# for image in os.listdir(directory):
+#     img2 = cv.imread(directory+image)
+#
+#     img3 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+#     cv2.imshow('test', img3)
+#     cv2.waitKey(0)
+#     rt, threshhold = cv.threshold(img3, 150,255,0)
+#     contur, hr = cv.findContours(threshhold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+#     arr = [contur[1].size, contur[1].min, contur[1].max , 255,0,0]
+#     dict[image] = arr
 
+
+img_rgb = cv.imread('test2.PNG')
+img_gray = cv.cvtColor(img_rgb,cv.COLOR_BGR2GRAY)
+template1 = cv.imread('temple/empty.PNG')
+template =  cv.cvtColor(template1,cv.COLOR_BGR2GRAY)
+w = template.shape[0]
+h = template.shape[1]
+res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 1)
+template1 = cv.imread('temple/1.PNG')
+template = cv.cvtColor(template1, cv.COLOR_BGR2GRAY)
+w = template.shape[0]
+h = template.shape[1]
+res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.9
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + 10, pt[1] + 10), (255, 0, 0), 1)
+
+
+template1 = cv.imread('temple/2.PNG')
+template = cv.cvtColor(template1, cv.COLOR_BGR2GRAY)
+w = template.shape[0]
+h = template.shape[1]
+res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 1)
+
+cv.imshow('test',img_rgb)
+cv.waitKey(0)
 imageMain =  cv2.imread('test.png')
 imageMainGray = cv.cvtColor(imageMain,cv.COLOR_BGR2GRAY)
 ret2, thresh2 = cv.threshold(imageMainGray, 101,255,0)
@@ -32,12 +68,14 @@ for cnt in contoursMain:
     if cnt.size == dict['1.PNG'][0]:
         cv.drawContours(imageMain, cnt, -1, (random.randint(0,255), random.randint(0,255),random.randint(0,255)), 3)
 
-        for i in range(0, len(cnt)):
-            x, y, w, h = cv2.boundingRect(cnt[i])
-            print(x)
     elif cnt.size == dict['2.PNG'][0]:
         cv.drawContours(imageMain, cnt, -1, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), 3)
-
-
+    elif cnt.size == dict['empty.PNG'][0]:
+        cv.drawContours(imageMain, cnt, -1, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), 3)
+        x = 0
+        y = 0
+        for i in range(0, len(cnt)):
+            x, y, w, h = cv2.boundingRect(cnt[i])
+        cv.circle(imageMain, (x, y), 10, (0, 0, 255), 5)
 cv2.imshow('test',imageMain)
 cv2.waitKey(0)
